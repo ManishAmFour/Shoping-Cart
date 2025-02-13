5af1c4be907f00d0a4542224542690b8cd442af9
ManishAmFour <ManishTewatia3020@gmail.com>
2025-02-13 18:59:05 +0530

    final change related responsiveness

--- src/components/differentLinks/CartDisplay.jsx
+++ src/components/differentLinks/CartDisplay.jsx
@@ -2,7 +2,11 @@ function CartNumberDisplay() {
   if (JSON.parse(localStorage.getItem("cart")) === null) {
     return (
       <div>
-        <p className="cart-display">Total Items in the Cart:- 0</p>
+        {window.innerWidth > 1000 ? (
+          <p className="cart-display">Total Items in the Cart:- 0</p>
+        ) : window.innerWidth < 1000 ? (
+          <div className="cart-display cart-display-no">0</div>
+        ) : null}
       </div>
     );
   } else {
